029801f14fcd75cb7100461a147d84d8e3303721
unknown <jonwk19@outlook.com>
2025-07-17 16:36:14 -0600

    NO-JIRA: Update products entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,4 +9,34 @@ export class Product {
   @Column('text', { unique: true })
   title: string;
 
+  @Column('numeric', {default: 0}) //Indica que es una calumna el tipo y luego configuraciones a ese campo
+  price: number;
+
+  // otra forma de definir es:
+  @Column({
+    type: 'text',
+    nullable: true
+})
+  description: string;
+
+  @Column('text', {
+    unique: true
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0
+  })
+  stock: number;
+
+  @Column('text',{
+    array: true
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  //tags
+  //images
 }
