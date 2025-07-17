a65ced7c4908fec74da90a253dae542d00efdbfe
unknown <jonwk19@outlook.com>
2025-07-17 15:49:51 -0600

    NO-JIRA: Add Nest connection to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true, // Cuando se hace un cambio en las entidades, se actualiza la base de datos automáticamente
+      logging: true, // Muestra los logs de las consultas SQL
+      autoLoadEntities: true, // Carga automáticamente las entidades definidas en el proyecto
+    })
+  ],
   controllers: [],
   providers: [],
 })
