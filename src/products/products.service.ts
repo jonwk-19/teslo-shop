7a1ac0ba1f5e1a2672aa1be355d641154bddc34a
unknown <jonwk19@outlook.com>
2025-07-18 17:33:22 -0600

    NO-JIRA: Add update product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -61,8 +61,21 @@ export class ProductsService {
       return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRespository.preload({
+      id: id,
+      ...updateProductDto
+    });
+
+    if (!product) throw new NotFoundException(`Product with id: ${id} not found`)
+
+    try {
+
+      await this.productRespository.save([product]);
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 
   async remove(id: string) {
