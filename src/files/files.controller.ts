f4f0d509031718eefaf63ad4ecd6d4d0897f5566
unknown <jonwk19@outlook.com>
2025-07-30 11:09:35 -0600

    NO-JIRA: Add upload images in file system

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,21 @@
 import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors( FileInterceptor('file'))
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter,
+    // limits: {fileSize: 1000},
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
+  }))
   uploadProductImage(
     @UploadedFile()  file: Express.Multer.File
   ){
