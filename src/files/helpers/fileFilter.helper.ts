cc26b8023a0ce89d8cac76edb44578ae495b3e69
unknown <jonwk19@outlook.com>
2025-07-30 10:20:22 -0600

    NO-JIRA: Add upload files is working

---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,21 @@
+import { FileFilterCallback } from 'multer';
+import { Request } from 'express';
+
+export const fileFilter = (
+  _req: Request,
+  file: Express.Multer.File,
+  callback: FileFilterCallback,
+): void => {
+  if (!file) {
+    return callback(new Error('File is empty'));
+  }
+
+  const fileExtension = file.mimetype.split('/')[1];
+  const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+  if (validExtensions.includes(fileExtension)) {
+    return callback(null, true);
+  }
+
+  callback(null, false);
+};
