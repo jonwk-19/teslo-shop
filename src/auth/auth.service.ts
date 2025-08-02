7121b9b0b128c5fc88c78544cb5ed330ba2e2961
unknown <jonwk19@outlook.com>
2025-08-02 01:43:36 -0600

    NO-JIRA: Add start to return jwt but just in the response, unuseful

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,13 +4,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from "bcrypt";
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService,
   ){}
 
 
@@ -23,11 +27,13 @@ export class AuthService {
         password: bcrypt.hashSync(password, 10)
       });
       await this.userRepository.save(user);
-      //Todo retunr jwtoken
 
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password: _, ...userWithoutPassword } = user;
-      return userWithoutPassword;
+      return {
+      ...userWithoutPassword,
+      token: this.getJwtToken({email: user.email})
+    };
     } catch (error) {
       this.handleDBErrors(error)
     }
@@ -46,8 +52,15 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid')
 
-    return user
-    //Todo retunr jwt
+    return {
+      ...user,
+      token: this.getJwtToken({email: user.email})
+    };
+  }
+
+  private getJwtToken(payload: JwtPayload){
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 
   private handleDBErrors (error: any): never{
