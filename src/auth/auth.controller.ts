9628ee7c9eccbaf8e0621852fd7c61d29ba879d7
unknown <jonwk19@outlook.com>
2025-08-04 14:11:41 -0600

    NO-JIRA: Add basics of guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto  } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser, RawHeaders } from './decorators';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -17,12 +19,23 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
-  @Get()
+  @Get('private')
   @UseGuards( AuthGuard() )
-  testingPrivateRoute(){
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+    // @GetUser('id', ParseUUIDPipe) id: User
+    // @Req() request: Express.Request,
+  ){
+    // console.log(request.user)
     return{
       ok: true,
-      message: 'Hola mundo private'
+      message: 'Hola mundo private',
+      user,
+      userEmail,
+      rawHeaders
     }
   }
+
 }
