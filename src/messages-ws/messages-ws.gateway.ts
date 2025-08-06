db2590e07cd74f3d2b0d4b8f8e9f5432a1886fd8
unknown <jonwk19@outlook.com>
2025-08-06 13:09:26 -0600

    NO-JIRA: Add hanlde if client connect or disconnect

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -4,6 +4,7 @@ import { Socket } from 'socket.io';
 
 @WebSocketGateway({cors: true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
