b3d24610064e8bac01b84e72b267170ccf567c93
unknown <jonwk19@outlook.com>
2025-08-06 13:46:52 -0600

    NO-JIRA: Add emit to send data to the clients

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,22 +1,27 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({cors: true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
+  @WebSocketServer() wss: Server; //? tine toda la info del server
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     // console.log('Client connected: ', client.id)
     this.messagesWsService.registerClient(client)
     console.log({connectados: this.messagesWsService.getConnectedClients()})
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients()) //? para decirle algo al cliente
   }
 
   handleDisconnect(client: Socket) {
     // console.log('Client disconnected: ', client.id)
     this.messagesWsService.removeClient(client.id)
     console.log({connectados: this.messagesWsService.getConnectedClients()})
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients()) //? para decirle algo al cliente
   }
 
 }
