bf9b0c01109a926ae9fc13287d8b11c87fc94e33
unknown <jonwk19@outlook.com>
2025-08-06 11:00:45 -0600

    NO-JIRA: Add postman collection and update swagger to darkmode

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,7 +24,14 @@ async function bootstrap() {
     // .addTag('cats') //? son agrupadores no los vamo a agragar directa mente aca por ahora
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const swaggerDarkTheme = readFileSync(
+    join(__dirname, '../api/swaggerDark.css'), // Ajusta ruta según dónde esté el archivo
+    'utf8',
+  )
+
+  SwaggerModule.setup('api', app, documentFactory, {
+    customCss: swaggerDarkTheme
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
